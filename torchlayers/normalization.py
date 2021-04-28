6fa983c4f740664b9978fa447474d40c7ec83485
klaudiapalasz <82610195+klaudiapalasz@users.noreply.github.com>
2021-04-28 20:27:35 +0200

    Update normalization.py

--- torchlayers/normalization.py
+++ torchlayers/normalization.py
@@ -11,27 +11,26 @@ class InstanceNorm(module.InferDimension):
     Based on input shape it either creates 1D, 2D or 3D instance normalization for inputs of shape
     3D, 4D, 5D respectively (including batch as first dimension).
 
-    Otherwise works like standard PyTorch's `InstanceNorm <https://pytorch.org/docs/stable/nn.html#torch.nn.InstanceNorm1d>`__
+    Otherwise works like standard PyTorch's `InstanceNorm [here](https://pytorch.org/docs/stable/nn.html#torch.nn.InstanceNorm1d)
 
-    Parameters
-    ----------
-    num_features : int
-        :math:`C` (number of channels in input) from an expected input.
-        Can be number of outputs of previous linear layer as well
-    eps : float, optional
-        Value added to the denominator for numerical stability.
-        Default: `1e-5`
-    momentum : float, optional
-        Value used for the `running_mean` and `running_var`
-        computation. Default: `0.1`
-    affine : bool, optional
-        If ``True``, this module has learnable affine parameters, initialized just like in batch normalization.
-        Default: ``False``
-    track_running_stats : bool, optional
-        If ``True``, this module tracks the running mean and variance,
-        and when set to ``False``, this module does not track such statistics and always uses batch
-        statistics in both training and eval modes.
-        Default: ``False``
+    Attributes:
+        num_features :
+            :math:`C` (number of channels in input) from an expected input.
+            Can be number of outputs of previous linear layer as well
+        eps :
+            Value added to the denominator for numerical stability.
+            Default: `1e-5`
+        momentum :
+            Value used for the `running_mean` and `running_var`
+            computation. Default: `0.1`
+        affine :
+            If ``True``, this module has learnable affine parameters, initialized just like in batch normalization.
+            Default: ``False``
+        track_running_stats :
+            If ``True``, this module tracks the running mean and variance,
+            and when set to ``False``, this module does not track such statistics and always uses batch
+            statistics in both training and eval modes.
+            Default: ``False``
 
     """
 
@@ -43,6 +42,27 @@ class InstanceNorm(module.InferDimension):
         affine: bool = False,
         track_running_stats: bool = False,
     ):
+        """Initialize `InstanceNorm` object.
+        
+        Arguments:
+            num_features :
+                :math:`C` (number of channels in input) from an expected input.
+                Can be number of outputs of previous linear layer as well
+            eps :
+                Value added to the denominator for numerical stability.
+                Default: `1e-5`
+            momentum :
+                Value used for the `running_mean` and `running_var`
+                computation. Default: `0.1`
+            affine :
+                If ``True``, this module has learnable affine parameters, initialized just like in batch normalization.
+                Default: ``False``
+            track_running_stats :
+                If ``True``, this module tracks the running mean and variance,
+                and when set to ``False``, this module does not track such statistics and always uses batch
+                statistics in both training and eval modes.
+                Default: ``False``
+        """
         super().__init__(
             dispatcher={
                 5: torch.nn.InstanceNorm3d,
@@ -63,28 +83,27 @@ class BatchNorm(module.InferDimension):
     Based on input shape it either creates `1D`, `2D` or `3D` batch normalization for inputs of shape
     `2D/3D`, `4D`, `5D` respectively (including batch as first dimension).
 
-    Otherwise works like standard PyTorch's `BatchNorm <https://pytorch.org/docs/stable/nn.html#batchnorm1d>`__.
+    Otherwise works like standard PyTorch's `BatchNorm [here](https://pytorch.org/docs/stable/nn.html#batchnorm1d)
 
-    Parameters
-    ----------
-    num_features : int
-        :math:`C` (number of channels in input) from an expected input.
-        Can be number of outputs of previous linear layer as well
-    eps : float, optional
-        Value added to the denominator for numerical stability.
-        Default: `1e-5`
-    momentum : float, optional
-        Value used for the `running_mean` and `running_var`
-        computation. Can be set to ``None`` for cumulative moving average
-        (i.e. simple average). Default: `0.1`
-    affine : bool, optional
-        If ``True``, this module has learnable affine parameters.
-        Default: ``True``
-    track_running_stats : bool, optional
-        If ``True``, this module tracks the running mean and variance,
-        and when set to ``False``, this module does not track such statistics and always uses batch
-        statistics in both training and eval modes.
-        Default: ``True``
+    Attributes:
+        num_features :
+            :math:`C` (number of channels in input) from an expected input.
+            Can be number of outputs of previous linear layer as well
+        eps :
+            Value added to the denominator for numerical stability.
+            Default: `1e-5`
+        momentum :
+            Value used for the `running_mean` and `running_var`
+            computation. Can be set to ``None`` for cumulative moving average
+            (i.e. simple average). Default: `0.1`
+        affine :
+            If ``True``, this module has learnable affine parameters.
+            Default: ``True``
+        track_running_stats :
+            If ``True``, this module tracks the running mean and variance,
+            and when set to ``False``, this module does not track such statistics and always uses batch
+            statistics in both training and eval modes.
+            Default: ``True``
 
     """
 
@@ -96,6 +115,28 @@ class BatchNorm(module.InferDimension):
         affine: bool = True,
         track_running_stats: bool = True,
     ):
+        """Initialize `BatchNorm` object.
+        
+        Arguments:
+            num_features :
+                :math:`C` (number of channels in input) from an expected input.
+                Can be number of outputs of previous linear layer as well
+            eps :
+                Value added to the denominator for numerical stability.
+                Default: `1e-5`
+            momentum :
+                Value used for the `running_mean` and `running_var`
+                computation. Can be set to ``None`` for cumulative moving average
+                (i.e. simple average). Default: `0.1`
+            affine :
+                If ``True``, this module has learnable affine parameters.
+                Default: ``True``
+            track_running_stats :
+                If ``True``, this module tracks the running mean and variance,
+                and when set to ``False``, this module does not track such statistics and always uses batch
+                statistics in both training and eval modes.
+                Default: ``True``
+        """
         super().__init__(
             dispatcher={
                 5: torch.nn.BatchNorm3d,
@@ -118,18 +159,17 @@ class GroupNorm(torch.nn.GroupNorm):
     Works exactly like PyTorch's counterpart, but `num_channels` is used as first argument
     so it can be inferred during first forward pass.
 
-    Parameters
-    ----------
-    num_channels : int
-        Number of channels expected in input
-    num_groups : int
-        Number of groups to separate the channels into
-    eps : float, optional
-        Value added to the denominator for numerical stability.
-        Default: `1e-5`
-    affine : bool, optional
-        If ``True``, this module has learnable affine parameters.
-        Default: ``True``
+    Attributes:
+        num_channels :
+            Number of channels expected in input
+        num_groups :
+            Number of groups to separate the channels into
+        eps :
+            Value added to the denominator for numerical stability.
+            Default: `1e-5`
+        affine :
+            If ``True``, this module has learnable affine parameters.
+            Default: ``True``
 
     """
 
@@ -140,6 +180,20 @@ class GroupNorm(torch.nn.GroupNorm):
         eps: float = 1e-05,
         affine: bool = True,
     ):
+        """Initialize `GroupNorm` object.
+        
+        Arguments:
+            num_channels :
+                Number of channels expected in input
+            num_groups :
+                Number of groups to separate the channels into
+            eps :
+                Value added to the denominator for numerical stability.
+                Default: `1e-5`
+            affine :
+                If ``True``, this module has learnable affine parameters.
+                Default: ``True``
+        """
         super().__init__(
             num_groups=num_groups, num_channels=num_channels, eps=eps, affine=affine,
         )
